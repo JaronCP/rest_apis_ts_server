e00b10d20cad80c9261a3035e2be761f167d2cc6
Jaron Cascante Pérez <jaroncp04@gmail.com>
2024-07-13 23:42:08 -0600

    cambios en db

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,8 +4,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const db = new Sequelize(process.env.DATABASE_URL!, {
-    models: [__dirname + '/../models/**/*'],
-    logging: false
+    models: [__dirname + '/../models/**/*']
 })
 
 export default db
